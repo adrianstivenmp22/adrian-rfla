7379bbb1bda6ec1518b4d206b22025b74486405c
Adrian Murillo <adrianstivenm@hotmail.com>
2025-05-20 22:16:57 -0500

    Title modificado

--- dashboard/inicio.php
+++ dashboard/inicio.php
@@ -10,7 +10,7 @@ if (empty($_SESSION['user_id'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Colegio Moderno - Dashboard</title>
+    <title>Colegio - Dashboard</title>
     
     <!-- Frameworks y librerías -->
     <link href="https://cdn.jsdelivr.net/npm/daisyui@3.9.4/dist/full.css" rel="stylesheet" type="text/css" />
